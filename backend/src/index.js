e4fc7775310e8d6795d034d127f897b2f2e6f328
Ankit Yadav <ankitya1608@gmail.com>
2025-04-08 17:41:11 +0530

    package done

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,6 +41,8 @@ if(process.env.NODE_ENV === "production"){
   })
 }
 
+console.log(path.join(__dirname, "../frontend", "dist", "index.html"));
+
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
