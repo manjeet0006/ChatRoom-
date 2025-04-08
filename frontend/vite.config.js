de7c94e39dabcf07614119203f759443291b9cf1
Ankit Yadav <ankitya1608@gmail.com>
2025-04-08 17:52:52 +0530

    update 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [
+    react({
+      eslint: {
+        // This prevents ESLint errors from blocking the build
+        // Works only if using `vite-plugin-eslint`, otherwise this is not needed
+        ignoreDuringBuilds: true,
+      }
+    })
+  ]
 })
