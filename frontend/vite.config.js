07ca6547216d9344cbc3de9c8ced110a4218c027
manjeet0006 <manjeetsingh.muz98@gmail.com>
2025-06-11 21:40:59 +0530

    fix: update vercel configuration for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      eslint: {
-        // This prevents ESLint errors from blocking the build
-        // Works only if using `vite-plugin-eslint`, otherwise this is not needed
-        ignoreDuringBuilds: true,
-      }
-    }),
-    tailwindcss(),
-  ]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
